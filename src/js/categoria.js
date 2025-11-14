f98fe5981405601b0076d499e7b64f14d559c2ed
NelsonPaltin <nrpaltin@uoc.edu>
2025-11-14 16:53:50 -0500

    Bug img 15

--- src/js/categoria.js
+++ src/js/categoria.js
@@ -3,9 +3,13 @@ import { senderos } from "./senderos.js";
 
 const contenedor = document.getElementById("senderos");
 
+
+
 senderos.forEach(lista => {
   const card = document.createElement("div");
   card.classList.add("card");
+
+  const imageUrl = new URL('../img/g4.jpeg?as=webp&width=250',import.meta.url);
 
   card.innerHTML = `
     <br>
@@ -13,7 +17,7 @@ senderos.forEach(lista => {
     <h2>${lista.nombre}</h2>
     <hr>
     <br>
-    <img src="${lista.img}" alt="Fotografía, ${lista.nombre}">
+    <img src="${imageUrl}" alt="Fotografía, ${lista.nombre}">
     <p class = "info-distancia"><Strong>Distancia:</strong> ${lista.km} km</p>
     <p>${lista.parr1}..</p>
     <p class = "caja-ver-mas"><a href ="${lista.page}" class = "enlace-ver-mas">...ver más</a></p>
