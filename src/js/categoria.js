bf1098da336bcd0dac1ba4fef79ca25a8e229e2e
NelsonPaltin <nrpaltin@uoc.edu>
2025-11-13 22:14:45 -0500

    primer commit base

---
+++ src/js/categoria.js
@@ -0,0 +1,24 @@
+import { senderos } from "./senderos.js";
+
+const contenedor = document.getElementById("senderos");
+
+senderos.forEach(lista => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  card.innerHTML = `
+    <br>
+    <hr>
+    <h2>${lista.nombre}</h2>
+    <hr>
+    <br>
+    <img src="${lista.img}" alt="Fotografía, ${lista.nombre}">
+    <p class = "info-distancia"><Strong>Distancia:</strong> ${lista.km} km</p>
+    <p>${lista.parr1}..</p>
+    <p class = "caja-ver-mas"><a href ="${lista.page}" class = "enlace-ver-mas">...ver más</a></p>
+    
+  `;
+
+  contenedor.appendChild(card);
+
+});
